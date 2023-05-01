bbe406c047ff56f98b4249b5f9a0ab314f65e384
S. J <solovyev833@gmail.com>
2023-05-01 18:32:54 +0300

    update test

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,5 +1,24 @@
 import App from "../samples/App";
 
 test("jsx works", () => {
-  expect(App).toEqual(["h1", { prop1: "prop value" }, "Hello, World!"]);
+  expect(App).toEqual({
+    children: [
+      {
+        children: [
+          { props: { nodeValue: "Hello, World!" }, type: "TEXT ELEMENT" }
+        ],
+        props: null,
+        type: "div"
+      },
+      {
+        children: [
+          { props: { nodeValue: "Hello, React!" }, type: "TEXT ELEMENT" }
+        ],
+        props: null,
+        type: "span"
+      }
+    ],
+    props: { prop1: "prop value" },
+    type: "h1"
+  });
 });
