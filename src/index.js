77de9336787918f598a1d816a2f5bf34d7f766f8
S. J <solovyev833@gmail.com>
2023-05-01 19:24:05 +0300

    add instantiate & reconcile

--- src/index.js
+++ src/index.js
@@ -11,37 +11,65 @@ const convertStringToTextElement = element =>
       }
     : element;
 
+const instantiate = element => {
+  const { type, props } = element;
+
+  const dom = isTextElement(element)
+    ? document.createTextNode("")
+    : document.createElement(type);
+
+  if (props) {
+    Object.keys(props)
+      .filter(isListener)
+      .forEach(name => {
+        const eventType = name.toLowerCase().substring(2);
+        dom.addEventListener(eventType, props[name]);
+      });
+
+    Object.keys(props)
+      .filter(isAttribute)
+      .forEach(name => {
+        dom[name] = props[name];
+      });
+  }
+
+  const childElements = element.children || [];
+
+  const childInstances = childElements.map(instantiate);
+  const childDoms = childInstances.map(childInstance => childInstance.dom);
+  childDoms.forEach(childDom => {
+    return dom.appendChild(childDom);
+  });
+  const instance = { dom, element, childInstances };
+  return instance;
+};
+
+const reconcile = (parentDom, instance, element) => {
+  if (instance == null) {
+    const newInstance = instantiate(element);
+    parentDom.appendChild(newInstance.dom);
+    return newInstance;
+  }
+
+  const newInstance = instantiate(element);
+  parentDom.replaceChild(newInstance.parent, instance.dom);
+  return newInstance;
+};
+
 class OwnReact {
+  constructor() {
+    this.rootInstance = null;
+  }
+
   static createElement(type, props, ...children) {
     const childElements = children.map(convertStringToTextElement);
     return { type, props, children: childElements };
   }
 
   static render(element, container) {
-    const { type, props } = element;
-
-    const parent = isTextElement(element)
-      ? document.createTextNode("")
-      : document.createElement(type);
-
-    if (props) {
-      Object.keys(props)
-        .filter(isListener)
-        .forEach(name => {
-          const eventType = name.toLowerCase().substring(2);
-          parent.addEventListener(eventType, props[name]);
-        });
-
-      Object.keys(props)
-        .filter(isAttribute)
-        .forEach(name => {
-          parent[name] = props[name];
-        });
-    }
-
-    const childElements = element.children || [];
-    childElements.forEach(childElement => this.render(childElement, parent));
-    container.appendChild(parent);
+    const prevInstance = this.rootInstance;
+    const nextInstance = reconcile(container, prevInstance, element);
+    this.rootInstance = nextInstance;
   }
 }
 
