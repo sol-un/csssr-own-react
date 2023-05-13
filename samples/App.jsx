c2c57380eddecd31e6f7e987befccbe019721165
S. J <solovyev833@gmail.com>
2023-05-13 11:33:19 +0300

    render alphabet, add shuffling logic

--- samples/App.jsx
+++ samples/App.jsx
@@ -1,12 +1,43 @@
 import OwnReact from "../src";
 // eslint-disable-next-line import/extensions
-import List from "../src/List.jsx";
+import Alphabet from "../src/List.jsx";
 
-const App = (
-  <List>
-    <div>Hello World</div>
-    <div>Hello React</div>
-  </List>
-);
+const letters = [
+  "а",
+  "б",
+  "в",
+  "г",
+  "д",
+  "е",
+  "ё",
+  "ж",
+  "з",
+  "и",
+  "й",
+  "к",
+  "л",
+  "м",
+  "н",
+  "о",
+  "п",
+  "р",
+  "с",
+  "т",
+  "у",
+  "ф",
+  "х",
+  "ц",
+  "ч",
+  "ш",
+  "щ",
+  "ъ",
+  "ы",
+  "ь",
+  "э",
+  "ю",
+  "я"
+];
+
+const App = <Alphabet letters={letters} />;
 
 export default App;
--- src/List.jsx
+++ src/List.jsx
@@ -1,15 +1,44 @@
 import OwnReact from ".";
 
-class List extends OwnReact.Component {
+const getRandomNumber = max => Math.floor(Math.random() * max);
+
+const shuffleElements = (arr, randomizer = getRandomNumber) => {
+  const shufflesAmount = getRandomNumber(10);
+  const copy = [...arr];
+
+  for (let i = 0; i < shufflesAmount - 1; i += 1) {
+    const first = randomizer(copy.length);
+    const second = randomizer(copy.length);
+    [copy[first], copy[second]] = [copy[second], copy[first]];
+  }
+  return copy;
+};
+
+class Alphabet extends OwnReact.Component {
   constructor(props) {
     super(props);
     this.props = props;
+    const { letters } = this.props;
+    this.state = { letters };
+  }
+
+  updateLetters() {
+    const { letters } = this.state;
+    const newState = shuffleElements(letters);
+    this.setState({ letters: newState });
   }
 
   render() {
-    const { children } = this.props;
-    return <div>{children}</div>;
+    const { letters } = this.state;
+    return (
+      // eslint-disable-next-line react/style-prop-object
+      <div style="display:flex;justify-content:space-around;">
+        {letters.map(letter => (
+          <div>{letter}</div>
+        ))}
+      </div>
+    );
   }
 }
 
-export default List;
+export default Alphabet;
